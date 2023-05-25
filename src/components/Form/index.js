2793edfa2c71b2a6c51786d3882084dd5c290f3c
Anastasiia Chetverykova <ana.chetverykova@gmail.com>
2023-05-25 12:18:21 +0200

    add submit event

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,16 +1,24 @@
 import React from "react";
-import { Button } from "../Button/index.js"
+import { Button } from "../Button/index.js";
 
-export function Form() {
+export function Form({ onAddActivity }) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+    console.log(data);
+    onAddActivity(data);
+    event.target.reset();
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h1>Add new activity</h1>
       <label htmlFor="form-input">Name</label>
-      <input name ="form-input" id="form-input"></input>
-      <label htmlFor="form-input">Good weather activity</label>
-      <input type="checkbox"></input>
-      <Button />
-
+      <input name="name" id="form-input"></input>
+      <label htmlFor="form-checkbox">Good weather activity</label>
+      <input type="checkbox" name="isForGoodWeather" id="form-checkbox"></input>
+      <Button  />
     </form>
   );
 }
