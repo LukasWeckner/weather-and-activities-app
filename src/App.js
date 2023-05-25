52bca4b58f65675d361382a2c1b0d14315c46b03
Tim Wagner <tim.w@gner-mail.de>
2023-05-25 16:28:31 +0200

    filter Listitems

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import { Form } from "./components/Form/index.js";
 import useLocalStorageState from "use-local-storage-state";
+import { useState } from "react";
 import { uid } from "uid";
 import { List } from "./components/List";
+
+export const isGoodWeather = false;
 
 function App() {
   const [activities, setActivities] = useLocalStorageState("activities", {
     defaultValue: [],
   });
+
+  const filteredActivities = activities.filter(
+    (activity) => activity.isForGoodWeather === isGoodWeather
+  );
+  console.log(filteredActivities);
+
   function handleAddActivity(activityObject) {
     setActivities([{ ...activityObject, id: uid() }, ...activities]);
     return activities;
   }
-  console.log(activities);
+
   return (
     <>
-      <List activities={activities} />
+      <List activities={filteredActivities} />
       <Form onAddActivity={handleAddActivity} />
     </>
   );
--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,14 +1,20 @@
 import "./List.css";
+import { isGoodWeather } from "../../App.js";
 
 export function List({ activities }) {
   return (
-    <ul className="list">
-      {activities.map((activity) => (
-        <li className="listItem" key={activity.id}>
-          {" "}
-          {activity.name}{" "}
-        </li>
-      ))}
-    </ul>
+    <>
+      <h2>
+        {isGoodWeather ? "the weather is awesome" : "bad weather, stay inside"}
+      </h2>
+      <ul className="list">
+        {activities.map((activity) => (
+          <li className="listItem" key={activity.id}>
+            {" "}
+            {activity.name}{" "}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
