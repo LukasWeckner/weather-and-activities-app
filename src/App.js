119b9a5d38f0b083bdcba49c7ba1f68ca6ba2a2f
Tim Wagner <tim.w@gner-mail.de>
2023-05-25 15:02:10 +0200

    add ListComponents

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import { Form } from "./components/Form/index.js";
-import { useState } from "react";
+import useLocalStorageState from "use-local-storage-state";
 import { uid } from "uid";
+import { List } from "./components/List";
 
 function App() {
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useLocalStorageState("activities", {
+    defaultValue: [],
+  });
   function handleAddActivity(activityObject) {
     setActivities([{ ...activityObject, id: uid() }, ...activities]);
     return activities;
   }
   console.log(activities);
-  return <Form onAddActivity={handleAddActivity} />;
+  return (
+    <>
+      <List activities={activities} />
+      <Form onAddActivity={handleAddActivity} />
+    </>
+  );
 }
 
 export default App;
